59559264fd2a5d95f85b85fb040f5dec64147193
mandi7469 <mandi7469@aol.com>
2024-08-29 20:17:15 -0400

    added form to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,20 +1,49 @@
 export default function Contact() {
-    return (
-      <div>
-        <h1>Contact Me</h1>
-        <p>
-          Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-          molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-          magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-          efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-          mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-          posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-          faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-          ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-          dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-          rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-        </p>
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="d-flex justify-content-center">Contact Me</h1>
       </div>
-    );
-  }
+      <div className="row pt-3">
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label message">
+            Name:
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleFormControlInput1" className="form-label message">
+            Email address:
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleFormControlInput1"
+          />
+        </div>
+        <div className="mb-3">
+          <label
+            for="exampleFormControlTextarea1"
+            className="form-label message"
+          >
+            Message:
+          </label>
+          <textarea
+            class="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          ></textarea>
+        </div>
+        <div class="col-12">
+          <button class="btn btn-outline-light" type="submit">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
