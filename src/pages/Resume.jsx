13f479d19156f7cd3021bbec5416c57f679bab09
mandi7469 <mandi7469@aol.com>
2024-08-30 18:25:40 -0400

    added downloadable pdf to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,3 +1,5 @@
+import pdf from '../assets/files/AmandaChangaResume.pdf'
+
 export default function Resume() {
   return (
     <div className="container">
@@ -5,33 +7,37 @@ export default function Resume() {
         <h1 className="d-flex justify-content-center">Resume</h1>
       </div>
       <div className="row pt-3">
-        <p>
-          <a href="#" className="link-underline d-flex justify-content-center" style={{ color: "#65AFFF" }}>
-            {" "}
-            <i class="bi bi-file-earmark-person p-1"></i>Download my resume
-          </a>
-        </p>
-        <div className="d-flex flex-column">
-        <h2>Front-End Proficiencies</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>JQuery</li>
-          <li>Bootstrap</li>
-          <li>React</li>
-          <li>Responsive Design</li>
-        </ul>
-        <h2>Back-End Proficiencies</h2>
-        <ul>
-          <li>APIs</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>Rest</li>
-          <li>PostgreSQL, Sequelize</li>
-          <li>MongoDB, Mongoose</li>
-          <li>GraphQL, Apollo</li>
-        </ul>
+        <a
+          href={pdf}
+          download="AmandaChangaResume.pdf"
+          style={{ color: "#65AFFF" }}
+          className="link-underline d-flex justify-content-center"
+        >
+          <i class="bi bi-file-earmark-person p-1"></i>Download my resume
+        </a>
+   
+
+        <div className="">
+          <h2>Front-End Proficiencies</h2>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>Bootstrap</li>
+            <li>React</li>
+            <li>Responsive Design</li>
+          </ul>
+          <h2>Back-End Proficiencies</h2>
+          <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>Rest</li>
+            <li>PostgreSQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>GraphQL, Apollo</li>
+          </ul>
         </div>
       </div>
     </div>
